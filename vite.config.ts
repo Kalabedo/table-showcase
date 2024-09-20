9932a897b525122ad6b16d5dc230b6ccb0824870
CodeWithThree <84151954+Cheung-KaWai@users.noreply.github.com>
2024-09-20 23:37:20 +0200

    :wrench: add glsl and tsconfigpaths plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import glsl from "vite-plugin-glsl";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react({
+      include: "**/*.glsl",
+    }),
+    glsl(),
+    tsconfigPaths(),
+  ],
+});
